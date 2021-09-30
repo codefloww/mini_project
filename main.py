669ee9006d7daeae83142992989649ce1166e6a0
codefloww <kryvpaul@gmail.com>
2021-09-30 16:42:46 +0300

    added a main.py with a sudo

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+bulls and cows
+Bulls and cows is a classic game about guessing a 4-digit 
+
+
+'''
+
+#generate a welcoming message
+#ask for a code
+#generate a code for a computer
+#start a guessing cycle
+#maintain output of info about guesses
+#when sb guessed exit loop and output winning info
+#ask to play again
+#when sb guessed exit loop and output winning info
