0b8c14cd8389366bb3c7e6e938ae9105f9e3c5e9
codefloww <kryvpaul@gmail.com>
2021-10-01 11:28:06 +0300

    minor changes

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 bulls and cows
 Bulls and cows is a classic game about guessing a 4-digit number.
 You are playing against computer.
-If you guessed a digit, but it's not in a proper position you have +1 cow.
-If you guessed a digit on its proper position you have +1 bull.
+If you guessed a digit, but it's not in a proper position you have 1 cow.
+If you guessed a digit on its proper position you have 1 bull.
 '''
 import random
 
@@ -15,6 +15,7 @@ def welcome():
     print("cows(if you guess a digit but not on its place) and bulls(if")
     print("a digit on it's proper place. You try to guess a code first.")
     print("if you want to finish enter 'q' or 'Q' in terminal")
+    print()
 
 
 def getcode():
@@ -24,11 +25,9 @@ def getcode():
     try:
         int(code)
     except:
-        print("It mustn't contain letters")
+        print("It must consists of digits")
         code=getcode()
-    if length==4:
-        print("Your code was saved")
-    else:
+    if length!=4:
         print("Your code hasn't 4 digit")
         code=getcode()
     for i in range(4):
@@ -69,7 +68,7 @@ def main():
     while True:
     #ask for a code
         user_code=getcode()
-
+        print("Your code was saved\n")
     #generate a code for a computer
         comp_code=gen_code()
 
@@ -77,10 +76,9 @@ def main():
         print("Lets start guessing!")
         guessing=True
         bulls_user=cows_user=bulls_comp=cows_user=0
-
+    
         while guessing:
             #player phase
-            print("Enter your guess")
             guess=getcode()
             bulls_user,cows_user=guess_result(guess,comp_code)
             #maintain output of info about guesses
@@ -88,17 +86,17 @@ def main():
             #computer phase
             bulls_comp,cows_comp=guess_result(gen_code(),user_code)
             #maintain output of info about guesses
-            print(f"Computer has {bulls_comp} bulls and {cows_comp} cows")
+            print(f"Computer has {bulls_comp} bulls and {cows_comp} cows\n")
             if bulls_user==4 or bulls_comp==4:
                 guessing=False
 
     #when sb guessed exit loop and output winning info
         if bulls_user==4 and bulls_comp==4:
-            print("It's draw!!!")
+            print("It's draw")
         elif bulls_comp==4:
             print("You lose(")
         else:
-            print("You win")
+            print("You win!")
 
     #ask to play again
         print("Want to play again?(if want to exit enter 'q' or 'Q')")
